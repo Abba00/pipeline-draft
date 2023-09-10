afa6aaeaf783e2cfa858cbc391d0af377167e61c
Abba00 <aaliconcern@gmail.com>
2023-09-10 10:41:22 +0100

    new requirements

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()   #execution info
@@ -23,5 +23,5 @@ class CustomException(Exception):
 #     try:
 #         a=1/0
 #     except Exception as e:
-#         logging.info("Divide by Zero")
+#         logging.ERROR("Divide by Zero")
 #         raise CustomException(e,sys)
